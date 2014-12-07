502a1902ec30f4d1ea57dd8b77974e2f68e6b637
chihsuan, haung <chihsuan.huang.7@gmail.com>
2014-12-07 09:03:27 +0800

    add evaluate.R

---
+++ Homework6/evaluate.R
@@ -0,0 +1,36 @@
+library(caret) 
+
+truth1 <- rawData[,1]
+truth1[truth1 == 'republican'] <- 1
+truth1[truth1 == 'democrat'] <- 2
+truth1 <- as.numeric(truth1)
+
+truth2 <- rawData[,1]
+truth2[truth2 == 'republican'] <- 2
+truth2[truth2 == 'democrat'] <- 1
+truth2 <- factor(as.numeric(truth2))
+
+cp <- cbind(rawData[1], truth1)
+cp <- cbind(cp, truth2)
+cp <- cbind(cp, clust.kmeans)
+cp <- cbind(cp, clust.hy)
+
+htable <- table(factor(clust.hy), factor(truth1))
+ktable <- table(factor(clust.kmeans), factor(truth1))
+as.numeric(cp[,2]) - as.numeric(cp[,3])
+as.numeric(cp[,2]) - as.numeric(cp[,4])
+
+confusionMatrix(htable)$overall[1]
+confusionMatrix(ktable)$overall[1]
+
+retrieved <- length(clust.kmeans)
+precision <- sum(clust.hy == truth1) / retrieved
+recall <- sum(clust.hy == truth1) / sum(truth1)
+Fmeasure <- 2 * precision * recall / (precision + recall)
+Fmeasure
+
+retrieved <- length(clust.kmeans)
+precision <- sum(clust.kmeans == truth1) / retrieved
+recall <- sum(clust.kmeans == truth1) / sum(truth1)
+Fmeasure <- 2 * precision * recall / (precision + recall)
+Fmeasure
